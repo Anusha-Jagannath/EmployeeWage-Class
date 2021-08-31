52dabc892311301b52d8a9914ddbbb8c97bb3b6c
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-31 13:56:03 +0530

    code added to calculate wages for a month

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -1,37 +1,40 @@
 package com.employee;
 
 public class Employee {
-	int isFullTime;
-	int isPartTime;
-	int wagePerHour;
-	int empWage;
-	int empHrs;
-	static int empCheck;
+	private int isFullTime;
+	private int isPartTime;
+	private int wagePerHour;
+	private int empWage;
+	private int empHrs;
+	private int noOfWorkingDays;
+	private int totalWage;
+	private static int empCheck;
 
 	Employee() {
 		isFullTime = 2;
 		isPartTime = 1;
 		wagePerHour = 20;
+		noOfWorkingDays = 20;
 		empWage = 0;
 		empHrs = 0;
+		totalWage = 0;
 	}
 
 	public void isPresent() {
-		empCheck = (int) Math.floor(Math.random() * 10 % 3);
-		switch (empCheck) {
-		case 2:
-			System.out.println("Employee is full time");
-			empHrs = 8;
-			break;
-		case 1:
-			System.out.println("Employee is part time");
-			empHrs = 4;
-			break;
-		default:
-			System.out.println("Employee is absent");
+		for (int day = 0; day < noOfWorkingDays; day++) {
+			empCheck = (int) Math.floor(Math.random() * 10 % 3);
+			switch (empCheck) {
+			case 2: empHrs = 8;
+					break;
+			case 1: empHrs = 4;
+					break;
+			default: empHrs = 0;
+
+			}
+			empWage = empHrs * wagePerHour;
+			totalWage += empWage;
 		}
-		empWage = empHrs * wagePerHour;
-		System.out.println("Employee wage " + empWage);
+		System.out.println("Total Employee wage " + totalWage);
 	}
 
 }
