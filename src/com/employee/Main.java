67cdd56d8cd8757bfa02364ccf5a22c667ff0108
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-31 13:08:19 +0530

    added code for employee is present ot absent

--- src/com/employee/Main.java
+++ src/com/employee/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation program");
+		Employee employee = new Employee();
+		employee.isPresent();
 	}
 
 }
