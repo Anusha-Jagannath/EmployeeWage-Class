e9998cbcaead1bc50c1317f4e07c2f52061a06f9
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-31 13:21:13 +0530

    employee wage code added

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -2,18 +2,30 @@ package com.employee;
 
 public class Employee {
 	int isFullTime;
+	int wagePerHour;
+	int empWage;
+	int empHrs;
 	double empCheck;
-	
-	Employee(){
+
+	Employee() {
 		isFullTime = 1;
+		wagePerHour = 20;
+		empWage = 0;
+		empHrs = 0;
 	}
 
 	public void isPresent() {
-		empCheck = Math.floor(Math.random()*10%2);
-		if( empCheck == isFullTime)
+		empCheck = Math.floor(Math.random() * 10 % 2);
+		if (empCheck == isFullTime) {
 			System.out.println("Employee is present");
-		else
+			empHrs = 8;
+		}
+		else {
 			System.out.println("Employee is absent");
+			empHrs = 4;
+		}
+		empWage = empHrs * wagePerHour; 
+		System.out.println("Employee wage "+empWage);
 	}
-	
+
 }
