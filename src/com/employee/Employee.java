f9539fae05518c2a68a04500c81ec880aea12d74
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-31 13:29:06 +0530

    Added: patime and full time employee wage

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -2,30 +2,34 @@ package com.employee;
 
 public class Employee {
 	int isFullTime;
+	int isPartTime;
 	int wagePerHour;
 	int empWage;
 	int empHrs;
 	double empCheck;
 
 	Employee() {
-		isFullTime = 1;
+		isFullTime = 2;
+		isPartTime = 1;
 		wagePerHour = 20;
 		empWage = 0;
 		empHrs = 0;
 	}
 
 	public void isPresent() {
-		empCheck = Math.floor(Math.random() * 10 % 2);
+		empCheck = Math.floor(Math.random() * 10 % 3);
 		if (empCheck == isFullTime) {
-			System.out.println("Employee is present");
+			System.out.println("Employee is full time");
 			empHrs = 8;
+		} else if(empCheck == isPartTime) {
+			System.out.println("Employee is part time");
+			empHrs = 4;
 		}
 		else {
 			System.out.println("Employee is absent");
-			empHrs = 4;
 		}
-		empWage = empHrs * wagePerHour; 
-		System.out.println("Employee wage "+empWage);
+		empWage = empHrs * wagePerHour;
+		System.out.println("Employee wage " + empWage);
 	}
 
 }
